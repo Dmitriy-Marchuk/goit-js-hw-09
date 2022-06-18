ee3a2fe4890c60331356f62695cd6b772596e0b8
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2022-06-18 14:39:25 +0300

    task-one-is-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    startBt: document.querySelector('button[data-start]'),
+    stopBt: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBt.addEventListener('click', onStartChangeColor);
+refs.stopBt.addEventListener('click', onStopChangeColor);
+
+
+function onStartChangeColor() {
+    console.log('старт');
+    
+    intervalId = setInterval(() => {
+        console.log(getRandomHexColor());
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startBt.disabled = true;
+};
+
+
+function onStopChangeColor() {
+    console.log('стоп');
+    
+    clearInterval(intervalId);
+    refs.startBt.disabled = false;
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
