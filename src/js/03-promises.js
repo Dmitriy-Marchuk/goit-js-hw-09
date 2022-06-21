53ed379ee54acc0635038302d248f422d2e7db5c
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2022-06-21 16:18:07 +0300

    hw-js-9-is-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,6 +2,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('form'),
+  amount: document.querySelector('[name="amount"]'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
 };
 
 refs.form.addEventListener('submit', handleSubmit);
@@ -9,11 +12,20 @@ refs.form.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
   e.preventDefault();
 
-  let amount = Number(e.currentTarget.amount.value);
-  let delay = Number(e.currentTarget.delay.value);
-  let step = Number(e.currentTarget.step.value);
+  // let amount = Number(e.currentTarget.amount.value);
+  // let delay = Number(e.currentTarget.delay.value);
+  // let step = Number(e.currentTarget.step.value);
 
-  for (let position = 0; position <= amount ; position++) {
+  let amount = parseInt(refs.amount.value);
+  let delay = parseInt(refs.delay.value);
+  let step = parseInt(refs.step.value);
+  
+  if (delay < 0 || amount < 0 || step < 0) {
+    Notify.failure('Error! All values must be bigger than 0 ');
+    return;
+  }
+
+  for (let position = 1; position <= amount ; position++) {
    createPromise(position, delay)
   .then(({ position, delay }) => {
     Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
@@ -29,6 +41,7 @@ function handleSubmit(e) {
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
+
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
 
@@ -40,6 +53,4 @@ function createPromise(position, delay) {
     }, delay)
   });
 };
-
-
 
