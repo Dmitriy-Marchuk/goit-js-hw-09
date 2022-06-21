01d65c8878bcdb55423e3048640abd5b3aba5a11
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2022-06-21 12:33:50 +0300

    hw-js-9-is-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  let amount = Number(e.currentTarget.amount.value);
+  let delay = Number(e.currentTarget.delay.value);
+  let step = Number(e.currentTarget.step.value);
+
+  for (let position = 0; position <= amount ; position++) {
+   createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
-}
+
+};
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay});
+      } else {
+        reject({ position, delay});
+      }
+    }, delay)
+  });
+};
+
+
+
